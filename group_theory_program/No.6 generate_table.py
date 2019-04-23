ac33cdafa5a4625e3cba120dce22b23e39169009
崔智皓 <47875329+cuizhihao0477@users.noreply.github.com>
2019-04-23 15:48:11 +0800

    generate_table
    化简的示例效果是在str尾端加一个a，为方便拼接，也可删去函数内部的化简步骤，用下面注释部分，在主函数挨个化简

---
+++ group_theory_program/No.6 generate_table.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 20:51:00 2019
+
+@author: 皇子皇孙
+"""
+import pandas as pd
+def generate_table(group=['r','f','rf']): #导入已知群
+    all=[] #用来保存所有组合
+    for i in group:
+        list=[] #用来保存每一个单位元与所有单位圆的组合
+        for j in group:
+            x=j+i
+            list=list+[x]
+        all.append(list)
+    df=pd.DataFrame({group[0]:all[0]}) #将所有组合排成表
+    for i in range(len(group)-1):
+        df[group[i+1]]=all[i+1]
+    df[' ']=group
+    df.set_index(' ',inplace=True)
+    return df
+def simplify(s): #示意化简函数，效果为在组合尾部加一个a，拼接时删除
+    return s+'a'
+table=generate_table()
+print('生成的表格:',/n,table)
+for i in range(len(table)):
+    for j in range(len(table)):
+        table.iloc[i,j]=simplify(table.iloc[i,j])
+print('化简后的表格:',/n,table)
